ba50454ac4dc65c2316e63a2ca5df6820fa94790
Huyen Duong <huyendt1911@gmail.com>
2014-08-11 16:53:52 +0700

    edit

--- sample/samplejar/src/main/java/com/telsoft/outsourcing/sample/samplejar/HelloBranch.java
+++ sample/samplejar/src/main/java/com/telsoft/outsourcing/sample/samplejar/HelloBranch.java
@@ -1,6 +1,6 @@
 package com.telsoft.outsourcing.sample.samplejar;
 
-public class HelloGit {
+public class HelloBranch {
 	public static void main( String[] args )
     {
         System.out.println( "Hello Git!!!!!" );
